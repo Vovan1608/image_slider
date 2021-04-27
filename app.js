ee7c9cd719e6e182e99affd2ba60e31b0d3f5ebf
Vovan1608 <57871837+Vovan1608@users.noreply.github.com>
2021-04-27 20:40:35 +0300

    added forEach

--- app.js
+++ app.js
@@ -4,3 +4,6 @@ const slides = document.querySelectorAll(".slide");
 const nextBtn = document.querySelector(".nextBtn");
 const prevBtn = document.querySelector(".prevBtn");
 
+slides.forEach((slide, idx) => {
+	slide.style.left = `${index * 100}px`;
+});
