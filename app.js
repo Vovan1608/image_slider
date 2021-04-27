b607a83710aa353f67f57ad6f07e8fd8aa2d75c4
Vovan1608 <57871837+Vovan1608@users.noreply.github.com>
2021-04-27 20:48:14 +0300

    added carousel()

--- app.js
+++ app.js
@@ -7,3 +7,21 @@ const prevBtn = document.querySelector(".prevBtn");
 slides.forEach((slide, idx) => {
 	slide.style.left = `${index * 100}px`;
 });
+
+let counter = 0;
+
+nextBtn.addEventListener("click", () => {
+	counter += 1;
+	carousel();
+});
+
+prevBtn.addEventListener("click", () => {
+	counter -= 1;
+	carousel();
+});
+
+const carousel = () => {
+	slides.forEach(slide => {
+		slide.style.transform = `translateX(-${counter * 100}%)`;
+	});
+}
