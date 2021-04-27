64b2ec4a8381fa197fc41bcccc527fb1aa692e68
Vovan1608 <57871837+Vovan1608@users.noreply.github.com>
2021-04-27 21:14:52 +0300

    done

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-"use strict";
+// "use strict";
 
 const slides = document.querySelectorAll(".slide");
 const nextBtn = document.querySelector(".nextBtn");
 const prevBtn = document.querySelector(".prevBtn");
 
-slides.forEach((slide, idx) => {
-	slide.style.left = `${index * 100}px`;
+slides.forEach((slide, index) => {
+	slide.style.left = `${index * 100}%`;
 });
 
 let counter = 0;
@@ -20,8 +20,15 @@ prevBtn.addEventListener("click", () => {
 	carousel();
 });
 
-const carousel = () => {
+function carousel() {
+
+	nextBtn.style.display = counter < slides.length - 1 ? "block" : "none";
+
+	prevBtn.style.display = counter > 0 ? "block" : "none";
+	
 	slides.forEach(slide => {
 		slide.style.transform = `translateX(-${counter * 100}%)`;
 	});
 }
+
+prevBtn.style.display = "none";
